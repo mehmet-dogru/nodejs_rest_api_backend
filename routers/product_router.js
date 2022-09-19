6bf630b320f6be6222a2dddf594cc105e7ac67c5
Mehmet Doğru <mdogru685@gmail.com>
2022-09-19 14:36:56 +0300

    token proc

--- routers/product_router.js
+++ routers/product_router.js
@@ -2,17 +2,18 @@ const express = require("express");
 const router = express.Router();
 const multerConfig = require("../config/multer_config");
 const Product = require("../models/Product");
-
 const productController = require("../contollers/product_controller");
+const authMiddleware = require("../middleware/authMiddleware");
+const adminMiddleware = require("../middleware/adminMiddleware");
 
-router.get("/", productController.getAllProducts);
+router.get("/", [authMiddleware], productController.getAllProducts);
 
-router.get("/:id", productController.productGetById);
+router.get("/:id", [authMiddleware], productController.productGetById);
 
-router.post("/", multerConfig.single("image"), productController.addProduct);
+router.post("/", [authMiddleware], multerConfig.single("image"), productController.addProduct);
 
-router.patch("/:id", multerConfig.single("image"), productController.updateProduct);
+router.patch("/:id",[authMiddleware], multerConfig.single("image"), productController.updateProduct);
 
-router.delete("/:id", productController.deleteProduct);
+router.delete("/:id", [authMiddleware], productController.deleteProduct);
 
 module.exports = router;
