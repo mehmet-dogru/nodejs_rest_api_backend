921c0a14d3ea74f057d792d874aa29e21ccfe40b
Mehmet Doğru <mdogru685@gmail.com>
2022-09-22 15:23:07 +0300

    heroku

--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -1,7 +1,8 @@
 const admin = (req, res, next) => {
   if (!req.user.isAdmin) {
     return res.status(403).json({
-      message: "Admin değilsiniz!!!",
+      statusCode: 403,
+      message: "Yönetici değilsiniz bu işlem gerçekleştirilemez!",
     });
   }
   next();
